c03366a6930ebe33aa3f524f94855617289c0ca0
Nikhil Menghani <nikhil@menghani.com>
2023-12-13 22:17:49 -0500

    Improve the appearance of FileItem

--- app/src/main/java/com/example/fileexplorer/ui/view/FileExplorerScreen.kt
+++ app/src/main/java/com/example/fileexplorer/ui/view/FileExplorerScreen.kt
@@ -1,7 +1,11 @@
 package com.example.fileexplorer.ui.view
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fileexplorer.ui.viewmodel.FileExplorerViewModel
 import java.io.File
 
@@ -36,17 +41,17 @@ fun FileExplorerScreen(files: List<File>, onFileClick: (File) -> Unit, onBack: (
 fun FileItem(
     file: File, onFileClick: (File) -> Unit
 ) {
-    val color = if (file.isDirectory) {
-        Color.Blue
-    } else {
-        Color.Red
-    }
+    val color = if (file.isDirectory) Color.Blue else Color.Red
     Text(
         text = file.name,
         color = color,
+        fontSize = 24.sp,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(vertical = 8.dp, horizontal = 16.dp) // Reduced vertical padding
+            .border(BorderStroke(1.dp, Color.Gray)) // Add border
+            .background(Color.LightGray.copy(alpha = 0.3f)) // Add background with some transparency
+            .padding(PaddingValues(8.dp)) // Padding inside the border
             .clickable { onFileClick(file) }
     )
 }
